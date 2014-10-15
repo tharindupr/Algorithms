133baa8ba0cbd6e6c9f2fda2134a17912e11b03f
tharindupr <tharindu.prf@gmail.com>
2014-10-15 19:47:26 +0530

    BFS DFS Dijesktra String Matching - KNP Boyer More Rabin Carp

---
+++ Graphs/DFS.py
@@ -0,0 +1,28 @@
+graph = {'A': set(['B', 'C']),
+         'B': set(['A', 'D', 'E']),
+         'C': set(['A', 'F']),
+         'D': set(['B']),
+         'E': set(['B', 'F']),
+         'F': set(['C', 'E'])}
+
+
+def dfs(graph, start):
+    visited, stack = set(), [start]
+    while stack:
+        vertex = stack.pop()
+        if vertex not in visited:
+            visited.add(vertex)
+            stack.extend(graph[vertex] - visited)
+    return visited
+
+dfs(graph, 'A') # {'E', 'D', 'F', 'A', 'C', 'B'}
+
+def dfs_recursive(graph, start, visited=None):
+    if visited is None:
+        visited = set()
+    visited.add(start)
+    for next in graph[start] - visited:
+        dfs(graph, next, visited)
+    return visited
+
+dfs(graph, 'C') # {'E', 'D', 'F', 'A', 'C', 'B'}
